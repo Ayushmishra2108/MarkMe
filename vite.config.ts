9a282002aa09f42ab39076a810e5d1460c9bcd4e
Ayush <aayu21082005@gmail.com>
2025-10-02 00:25:58 +0530

    Add sharp dependency and mark as external in Vite config for Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,9 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     outDir: "dist/spa",
+    rollupOptions: {
+      external: ["sharp"],
+    },
   },
   plugins: [react(), expressPlugin()],
   resolve: {
